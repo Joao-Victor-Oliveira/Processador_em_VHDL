dc1158dae6397a32b82cff0c25f8869bf4ecd402
aguaym <63931923+aguaym@users.noreply.github.com>
2025-10-09 22:02:14 -0300

    Add files via upload
    ROM de leitura assincrona e endereco de memorias para 7 bits (vetor de 127).

--- source/ROM.vhd
+++ source/ROM.vhd
@@ -2,33 +2,29 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 entity rom is
-   port( clk      : in std_logic;
-         endereco : in unsigned(6 downto 0);
-         dado     : out unsigned(11 downto 0) 
+   port( 
+         endereco : in unsigned(6 downto 0);  --7  bits
+         dado     : out unsigned(13 downto 0) --14 bits
    );
 end entity;
 architecture a_rom of rom is
-   type mem is array (0 to 10) of unsigned(11 downto 0);
+   type mem is array (0 to 127) of unsigned(13 downto 0);
    constant conteudo_rom : mem := (
       -- caso endereco => conteudo
-      0  => "000000000010",
-      1  => "100000000000",
-      2  => "000000000000",
-      3  => "000000000000",
-      4  => "100000000000",
-      5  => "000000000010",
-      6  => "111100000011",
-      7  => "000000000010",
-      8  => "000000000010",
-      9  => "000000000000",
-      10 => "000000000000",
+      0  => "00000000000010",
+      1  => "00100000000000",
+      2  => "00000000000000",
+      3  => "00000000000000",
+      4  => "00100000000000",
+      5  => "00000000000010",
+      6  => "00111100000011",
+      7  => "00000000000010",
+      8  => "00000000000010",
+      9  => "00000000000000",
+      10 => "00000000000000",
       others => (others=>'0')
    );
 begin
-   process(clk)
-   begin
-      if(rising_edge(clk)) then
-         dado <= conteudo_rom(to_integer(endereco));
-      end if;
-   end process;
+   --Leitura assincrona
+   dado <= conteudo_rom(to_integer(endereco));
 end architecture;
