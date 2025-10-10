eedd5e84ee1960feec9fbcca9e3fb3c36ec9922f
plugado <jvoobmep@gmail.com>
2025-10-10 15:11:17 -0300

    ajustes Banco registradores

--- source/BancoRegistradores.vhd
+++ source/BancoRegistradores.vhd
@@ -21,24 +21,61 @@ entity BancoRegistradores is
 end BancoRegistradores;
 
 architecture Behavioral of BancoRegistradores is
-    type reg_vetor is array (0 to 5) of std_logic_vector(15 downto 0);
-    signal regs : reg_vetor := (others => (others => '0'));
+    signal reg_0 : std_logic_vector(15 downto 0) := (others => '0');
+    signal reg_1 : std_logic_vector(15 downto 0) := (others => '0');
+    signal reg_2 : std_logic_vector(15 downto 0) := (others => '0');
+    signal reg_3 : std_logic_vector(15 downto 0) := (others => '0');
+    signal reg_4 : std_logic_vector(15 downto 0) := (others => '0');
+    signal reg_5 : std_logic_vector(15 downto 0) := (others => '0');
+
 begin
 
     process(clk, rst)
     begin
         if rst = '1' then
-            regs <= (others => (others => '0'));
+            reg_0 <= "0000000000000000";
+            reg_1 <= "0000000000000000";
+            reg_2 <= "0000000000000000";
+            reg_3 <= "0000000000000000";
+            reg_4 <= "0000000000000000";
+            reg_5 <= "0000000000000000";
         elsif rising_edge(clk) then
             if wr_en = '1' then
                 if to_integer(addr_wr) < 6 then
-                    regs(to_integer(addr_wr)) <= entrada;
+                    if addr_wr = "000" then
+                        reg_0 <= entrada;
+                    elsif addr_wr = "001" then
+                        reg_1 <= entrada;
+                    elsif addr_wr = "010" then
+                        reg_2 <= entrada;
+                    elsif addr_wr = "011" then
+                        reg_3 <= entrada;
+                    elsif addr_wr = "100" then
+                        reg_4 <= entrada;
+                    elsif addr_wr = "101" then
+                        reg_5 <= entrada;
+                    end if;
                 end if;
             end if;
         end if;
     end process;
 
-    saida1 <= regs(to_integer(addr_rd1)) when to_integer(addr_rd1) < 6 else (others => '0');
-    saida2 <= regs(to_integer(addr_rd2)) when to_integer(addr_rd2) < 6 else (others => '0');
+    with addr_rd1 select
+        saida1 <= reg_0 when "000",
+        reg_1 when "001",
+        reg_2 when "010",
+        reg_3 when "011",
+        reg_4 when "100",
+        reg_5 when "101",
+        (others => '0') when others; 
+    
+    with addr_rd2 select
+        saida2 <= reg_0 when "000",
+        reg_1 when "001",
+        reg_2 when "010",
+        reg_3 when "011",
+        reg_4 when "100",
+        reg_5 when "101",
+        (others => '0') when others;
 
 end Behavioral;
