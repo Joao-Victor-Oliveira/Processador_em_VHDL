ac255d45cd6acfab5a8864f34a9d486d66ed40e2
plugado <jvoobmep@gmail.com>
2025-11-07 15:19:22 -0300

    minor changes

--- source/ROM.vhd
+++ source/ROM.vhd
@@ -47,9 +47,9 @@ architecture a_rom of rom is
       -- E. Se R3<30 salta para a instrução do passo C (Endereço 5)
       -- 12: BLT R2, -7  ; Salta para 5 se ACC (R3) < R6 (30)
       --    PC_Atual = 12, Alvo = 5
-      --    Offset = Alvo - (PC_Atual + 1) = 5 - 13 = -8
+      --    Offset = Alvo - (PC_Atual + 1) = 5 - 12 = -7
       --    R2 = "010", -7 (C2, 7 bits) = "1111000"
-      12 => "01110101111010",
+      12 => "01110101111000",
       
       -- F. Copia valor de R4 para R5
       -- 13: LDA R4 ("100")
